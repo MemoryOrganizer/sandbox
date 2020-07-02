1ba8287601d034d01c873a3fd16e4a728e48008d
Jaime Sanders <jaimelyn517@gmail.com>
2020-07-02 14:23:24 -0700

    hooks up Cloudinary

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
 const multer = require('multer');
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function(req, file, cb) {
-    console.log(file);
-    cb(null, file.originalname);
-  }
-});
+const cloudinary = require('cloudinary').v2;
 
-app.post('/upload', (req, res, next) => {
-  const upload = multer({ storage }).single('name-of-input-key');
-  upload(req, res, function(err) {
-    if(err) {
-      return res.send(err);
-    }
-    res.json(req.file);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./'));
+
+const photoUpload = multer({ dest: 'uploaded/' }).single('photo');
+
+app.post('/', photoUpload, (req, res, next) => {
+  console.log(req.body);
+  console.log(req.file);
+
+  cloudinary.config({
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET,
+    cloud_name: process.env.CLOUDINARY_NAME
   });
+
+  const path = req.file.path;
+  const uniqueFilename = new Date().toISOString();
+
+  cloudinary.uploader.upload(
+    path,
+    { public_id: `blog/${uniqueFilename}`, tags: 'blog' }, // directory and tags are optional
+    function(err, image) {
+      if(err) return res.send(err);
+      console.log('file uploaded to Cloudinary');
+      // remove file from server
+      const fs = require('fs');
+      fs.unlinkSync(path);
+      // return image details
+      res.json(image);
+    }
+  );
 });
 
 app.listen(5500);
-
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use(express.static('./'));
-
-// const photoUpload = multer({ dest: 'uploaded/' }).single('photo');
-
-// app.post('/', photoUpload, (req, res, next) => {
-//   console.log(req.body);
-//   console.log(req.file);
-// });
-
-
