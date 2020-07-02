a72ffb1245d4977e432962e80e9f5948828ea9f8
Jaime Sanders <jaimelyn517@gmail.com>
2020-07-02 13:47:31 -0700

    gets photo to upload via postman

--- index.js
+++ index.js
@@ -2,15 +2,37 @@ const express = require('express');
 const app = express();
 
 const multer = require('multer');
-const photoUpload = multer({ dest: 'uploaded/' }).single('photo');
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb) {
+    console.log(file);
+    cb(null, file.originalname);
+  }
+});
 
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static('./'));
-
-app.post('/', photoUpload, (req, res, next) => {
-  console.log(req.body);
-  console.log(req.file);
+app.post('/upload', (req, res, next) => {
+  const upload = multer({ storage }).single('name-of-input-key');
+  upload(req, res, function(err) {
+    if(err) {
+      return res.send(err);
+    }
+    res.json(req.file);
+  });
 });
 
 app.listen(5500);
+
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(express.static('./'));
+
+// const photoUpload = multer({ dest: 'uploaded/' }).single('photo');
+
+// app.post('/', photoUpload, (req, res, next) => {
+//   console.log(req.body);
+//   console.log(req.file);
+// });
+
+
